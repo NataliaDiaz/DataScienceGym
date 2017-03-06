dbca9f2faf8e68c327aab46b913753fb8f7beb27
Natalia Diaz Rodriguez <diaz.rodriguez.natalia@gmail.com>
2017-03-06 20:21:26 +0100

    fixing total val for max val

--- max-players/max-nr-players-can-touch-ball.py
+++ max-players/max-nr-players-can-touch-ball.py
@@ -1,4 +1,31 @@
 import sys
+"""
+Football players in a field can throw a ball between them and we need to find the max nr of players that
+can touch a ball after the ball is given to any player at the begining (each player can touch the ball
+an unlimited nr of times). If each input in the file contains a comma-separated set of names where the
+first field on each line is the name of a player (as a string), and the remaining fields are the names
+of the players that that player can see:
+
+player1,visible_player_1,visible_player_2,...
+player2,visible_player_1,visible_player_2,...
+...
+
+For example, given the following input:
+George,Beth, Sue
+Rick,Anne
+Anne,Beth
+Beth, Anne ,George
+Sue,Beth
+The program should print 3 because George can see Beth and Beth can see George.
+Additionally, Beth can see Anne, and Anne can see Beth. However, despite Rick being able to
+see Anne, Anne cannot see Rick, and despite George being able to see Sue, Sue cannot see
+George.
+
+O(n*m) should be the worse case complexity (n is the nr of rows in the input file, m is the max number
+of players that a single player can see).
+
+ToDo: it is max, not total of players, therefore implement with tree traversal and class.
+"""
 
 def get_min_players_can_touch_ball(inputfile='./data/players.csv'):
     n = 0
@@ -6,6 +33,7 @@ def get_min_players_can_touch_ball(inputfile='./data/players.csv'):
     m = []
     visibles = {}
     useful_players = set()
+    options = []
     with open(inputfile,'r') as f:
         output = f.read()
     # Create adjacency matrix within a dictionary
@@ -17,19 +45,26 @@ def get_min_players_can_touch_ball(inputfile='./data/players.csv'):
             names = [x for x in names if len(x)>0]
             name, visible_players = names[0], names[1:]
             if name in visibles:
-                print "Wrong format in input file: each player must appear at the beginning of each line only once"
-                sys.exit(-1)
+                visibles[name].extend(visible_players)
+            else:
+                visibles[name] = visible_players
             if not ',' in line:
                 print "Wrong format in input file: each line must be separated by commas if that player is seeing any other player"
-                sys.exit(-1)
-            visibles[name] = visible_players
-    print "Finding maximum for players: \n",visibles
-    # Finding pairs of
+                #print -1;return -1#sys.exit(-1)
+
+    print "Finding maximum nr of players that can have the ball: \n",visibles
+    # Finding pairs of players for each connected component (or subgraph of connected players 2-to-2)
+    processed = []
     for player in visibles.keys():
+        #if not player in processed: # TODO TRAVERSAL TREE
+        processed.append(player)
         for visible_player in visibles[player]:
             if visible_player in visibles and player in visibles[visible_player]:
                 useful_players.add(player)
-    n = len(useful_players)
+        # we add the connected (nr of visible 2-to-2) players in this subgraph
+        options.append(len(useful_players))
+        useful_players.clear()
+    n = max(options)
     print n
     return n
 print "\n\nTest 1"
@@ -41,7 +76,10 @@ get_min_players_can_touch_ball('./data/players2.csv') # 6
 print "\n\nTest 4"
 get_min_players_can_touch_ball('./data/players3.csv') # 3
 print "\n\nTest 5"
-get_min_players_can_touch_ball('./data/players4.csv') # Error, wrong input
+get_min_players_can_touch_ball('./data/players4.csv') # 3
+print "\n\nTest 6"
+get_min_players_can_touch_ball('./data/players5.csv') # 3
+
 
 """
 Other possibility would be:
